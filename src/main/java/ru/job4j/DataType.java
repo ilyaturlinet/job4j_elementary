f1ffd6df779d736f49c6e363b14198d9c6f34c52
ilyaturlinet <ilya_turlinet@mail.ru>
2022-06-14 17:08:13 +0300

    Арифметические операции. [#185]

---
+++ src/main/java/ru/job4j/DataType.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+public class DataType {
+    //Age. Max 125 years
+    byte age;
+    //Gender
+    char gender;
+    //Height
+    float height;
+    //Bricks. Can be in half
+    double bricks;
+    //Bacteria
+    long bacteria;
+    //Distance to space objects
+    long distance;
+    //Permit
+    boolean permit;
+    //File size
+    long size;
+}
